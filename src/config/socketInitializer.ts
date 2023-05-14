2daef09f152544612ca6b14180ed26ea9d326805
Anoop Chandran <mailto.anoopc@gmail.com>
2023-05-14 20:47:30 +0800

    debug logs in place

--- src/config/socketInitializer.ts
+++ src/config/socketInitializer.ts
@@ -1,7 +1,9 @@
 import io from "socket.io-client";
-const socket_domain = process.env.SOCKET_DOMAIN ?? "http://localhost:3000"
+const socket_domain = import.meta.env.VITE_SOCKET_DOMAIN ?? "http://localhost:3000"
 export const socket = io(socket_domain);
 
 export const joinRoom = (data: any) => {
+  console.log(socket_domain, "socket_domain________________________________________________");
+  
   socket.emit("room-join", data);
 };
