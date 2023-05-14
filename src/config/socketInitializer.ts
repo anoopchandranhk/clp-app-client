75c460a16dcf651f0fd2e95330ce3b1cc32b7f1f
Anoop Chandran <mailto.anoopc@gmail.com>
2023-05-14 20:18:03 +0800

    socket domain variable set

--- src/config/socketInitializer.ts
+++ src/config/socketInitializer.ts
@@ -1,6 +1,6 @@
 import io from "socket.io-client";
-
-export const socket = io("http://localhost:3000");
+const socket_domain = process.env.SOCKET_DOMAIN ?? "http://localhost:3000"
+export const socket = io(socket_domain);
 
 export const joinRoom = (data: any) => {
   socket.emit("room-join", data);
